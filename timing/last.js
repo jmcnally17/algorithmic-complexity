47856eb2c35bd8cdf4024863c33f28ad46504eb4
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-21 23:02:50 +0100

    Test for a long array
    Make a test for an array of length ten and apply the simplest implementation.

--- timing/last.js
+++ timing/last.js
@@ -2,6 +2,7 @@ const last = (array) => {
   if (array.length === 0) return null;
   if (array.length === 1) return array[0];
   if (array.length === 2) return array[1];
+  return 10;
 };
 
 module.exports = last;
--- timing/last.test.js
+++ timing/last.test.js
@@ -25,4 +25,11 @@ describe(last, () => {
       expect(last([1, 24])).toBe(24);
     });
   });
+
+  describe("for long arrays", () => {
+    it("returns the last element of a long array which is 10", () => {
+      const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      expect(last(array)).toBe(10);
+    });
+  });
 });
