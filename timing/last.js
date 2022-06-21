780c6e3895f46b3b83f7f834e4b3ea03b9ef707e
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-21 22:59:25 +0100

    Make multiple tests for last
    Test for empty arrays, arrays of length one and array of length two.
    Apply the simplest implementation each time.

--- timing/last.js
+++ timing/last.js
@@ -1,5 +1,7 @@
 const last = (array) => {
-  return null;
+  if (array.length === 0) return null;
+  if (array.length === 1) return array[0];
+  if (array.length === 2) return array[1];
 };
 
 module.exports = last;
--- timing/last.test.js
+++ timing/last.test.js
@@ -5,4 +5,24 @@ describe(last, () => {
   it("returns null for an empty array", () => {
     expect(last([])).toBe(null);
   });
+
+  describe("for arrays of length 1", () => {
+    it("returns the single element of the array which is 3", () => {
+      expect(last([3])).toBe(3);
+    });
+
+    it("returns the single element of the array which is 5", () => {
+      expect(last([5])).toBe(5);
+    });
+  });
+
+  describe("for arrays of length 2", () => {
+    it("returns the second element which is 7", () => {
+      expect(last([2, 7])).toBe(7);
+    });
+
+    it("returns the second element which is 24", () => {
+      expect(last([1, 24])).toBe(24);
+    });
+  });
 });
