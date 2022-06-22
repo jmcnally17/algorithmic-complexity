26c69a24d1414f71e9feb7f2dd69a94f76cf0524
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-22 22:06:46 +0100

    Plot graph for reverse()
    Plot the graph for the reverse() function on arrays by using Chart.js.

---
+++ timing/array-functions/reverse/plotReverse.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+const Chart = require("chart.js");
+const saveData = require("../saveData");
+
+const xValues = [];
+for (i = 1; i < 21; i++) {
+  xValues.push(5000 * i);
+}
+
+const yValues = saveData(1);
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        data: yValues,
+        borderColor: "red",
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        font: {
+          size: 32,
+        },
+        text: "reverse()",
+      },
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          font: {
+            size: 20,
+          },
+          text: "Array length /# of elements",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          font: {
+            size: 20,
+          },
+          text: "Time /ms",
+        },
+      },
+    },
+  },
+});
