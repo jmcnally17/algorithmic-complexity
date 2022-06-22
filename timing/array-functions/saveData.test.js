dad666d59b7d49f395ca84924c64761b01409824
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-22 21:54:41 +0100

    Typo

--- timing/array-functions/saveData.test.js
+++ timing/array-functions/saveData.test.js
@@ -2,7 +2,7 @@
 const saveData = require("./saveData");
 
 describe(saveData, () => {
-  it("saves 20 pairs of data points for plotting on a graph for timing reverse()", () => {
+  it("saves 20 pairs of data points for plotting on a graph for timing a function", () => {
     const mockCallbackOne = jest.fn();
     const mockCallbackTwo = jest.fn();
     expect(saveData(1, mockCallbackOne, mockCallbackTwo).length).toBe(20);
