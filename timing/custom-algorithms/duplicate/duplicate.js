7502d0498d749e20fa2e98bafd0a7e2adef5a68c
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-26 21:19:44 +0100

    Finish duplicate() functionality
    Modify duplicate() to account for an array of any length with any amount
    of repeating elements. Make a slight change to the output of the expect
    statements by returning a list of strings.
    
    Also, add in an extra test for a long array for extra checking.

--- timing/custom-algorithms/duplicate/duplicate.js
+++ timing/custom-algorithms/duplicate/duplicate.js
@@ -1,8 +1,18 @@
 const duplicate = (array) => {
-  if (array[1] === 3 && array[3] === 3) {
-    return [3];
+  let repeats = [];
+  let counts = {};
+  array.forEach((element) => {
+    if (counts[element] === undefined) {
+      counts[element] = 1;
+    } else {
+      counts[element]++;
+    }
+    console.log(counts);
+  });
+  for (const [key, value] of Object.entries(counts)) {
+    if (value > 1) repeats.push(key);
   }
-  return [];
+  return repeats;
 };
 
 module.exports = duplicate;
--- timing/custom-algorithms/duplicate/duplicate.test.js
+++ timing/custom-algorithms/duplicate/duplicate.test.js
@@ -7,6 +7,15 @@ describe(duplicate, () => {
   });
 
   it("returns an array containing the number 3 for an array where 3 repeats", () => {
-    expect(duplicate([1, 3, 5, 3])).toEqual([3]);
+    expect(duplicate([1, 3, 5, 3])).toEqual(["3"]);
+  });
+
+  it("returns an array of 2 repeating elements in the array", () => {
+    expect(duplicate([1, 2, 3, 1, 4, 4, 6])).toEqual(["1", "4"]);
+  });
+
+  it("returns the array of repeating elements for a long array", () => {
+    const arr = [1, 2, 55, 4, 2, 4, 34, 55, 6, 7, 7, 8, 8, 9, 1, 55];
+    expect(duplicate(arr)).toEqual(["1", "2", "4", "7", "8", "55"]);
   });
 });
