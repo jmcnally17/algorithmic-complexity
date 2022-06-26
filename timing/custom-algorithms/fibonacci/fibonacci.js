8402a02271b3028c00a2090d0a45e114e580810f
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-26 22:04:04 +0100

    Pass test for 2 fibonacci numbers
    Use the simplest implementation for entering 2 as a parameter to return
    the first 2 fibonacci numbers.

--- timing/custom-algorithms/fibonacci/fibonacci.js
+++ timing/custom-algorithms/fibonacci/fibonacci.js
@@ -1,4 +1,5 @@
 const fibonacci = (num) => {
+  if (num === 2) return [0, 1];
   if (num === 1) return [0];
   return [];
 };
--- timing/custom-algorithms/fibonacci/fibonacci.test.js
+++ timing/custom-algorithms/fibonacci/fibonacci.test.js
@@ -9,4 +9,8 @@ describe(fibonacci, () => {
   it("returns the number 0 in an array for 1 fibonacci number", () => {
     expect(fibonacci(1)).toEqual([0]);
   });
+
+  it("returns 0 and 1 for 2 fibonacci numbers", () => {
+    expect(fibonacci(2)).toEqual([0, 1]);
+  });
 });
