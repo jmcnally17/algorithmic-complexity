0b229569cdb2b7d81d9b60612d55325fa4c2d996
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-26 21:24:31 +0100

    Refactor
    Extract two methods from duplicate to make code more readable and keep
    functions shorter.

--- timing/custom-algorithms/duplicate/duplicate.js
+++ timing/custom-algorithms/duplicate/duplicate.js
@@ -1,18 +1,25 @@
 const duplicate = (array) => {
   let repeats = [];
   let counts = {};
+  addToCounts(array, counts);
+  addToRepeats(counts, repeats);
+  return repeats;
+};
+
+const addToCounts = (array, counts) => {
   array.forEach((element) => {
     if (counts[element] === undefined) {
       counts[element] = 1;
     } else {
       counts[element]++;
     }
-    console.log(counts);
   });
+};
+
+const addToRepeats = (counts, repeats) => {
   for (const [key, value] of Object.entries(counts)) {
     if (value > 1) repeats.push(key);
   }
-  return repeats;
 };
 
 module.exports = duplicate;
