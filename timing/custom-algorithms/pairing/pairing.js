d626484fda288f30410d5460a1bc2ad88fc9a3be
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-26 23:16:49 +0100

    Pass test for a class of one student
    Modify pairing() to return one student working alone.

--- timing/custom-algorithms/pairing/pairing.js
+++ timing/custom-algorithms/pairing/pairing.js
@@ -1,4 +1,5 @@
 const pairing = (array) => {
+  if (array.length === 1) return [[array[0]]];
   return [];
 };
 
--- timing/custom-algorithms/pairing/paring.test.js
+++ timing/custom-algorithms/pairing/paring.test.js
@@ -5,4 +5,8 @@ describe(pairing, () => {
   it("returns an empty list for an empty set of students", () => {
     expect(pairing([])).toEqual([]);
   });
+
+  it("returns an array of one pairing array of one person for a set of one student", () => {
+    expect(pairing(["John"])).toEqual([["John"]]);
+  });
 });
