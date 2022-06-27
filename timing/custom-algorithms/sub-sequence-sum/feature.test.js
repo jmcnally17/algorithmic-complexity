4d38b39b347e6bf5fd15139bd4b1c5b6dd9a31c6
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-27 13:46:14 +0100

    Skip subSequenceSum() feature
    Skip the feature test as the test suite takes far too long when running
    test:coverage.

--- timing/custom-algorithms/sub-sequence-sum/feature.test.js
+++ timing/custom-algorithms/sub-sequence-sum/feature.test.js
@@ -2,7 +2,7 @@
 const saveData = require("../saveData");
 const subSequenceSum = require("./subSequenceSum");
 
-test("creates a set of 20 data points for timing fibonacci()", () => {
+xtest("creates a set of 20 data points for timing fibonacci()", () => {
   const data = saveData(subSequenceSum, 1);
   expect(data.length).toBe(20);
 });
