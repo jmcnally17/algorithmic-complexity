8d4052e51f3488836ae32ecfd74fdfd7d2a9fcee
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-30 10:48:58 +0100

    Update README

--- custom-algorithms/createDataPoint.js
+++ custom-algorithms/createDataPoint.js
@@ -1,12 +1,12 @@
-const createDataPoint = (input, callback, num, target) => {
+const createDataPoint = (input, callback, numberOfMeasurements, target) => {
   let cumulative = 0;
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < numberOfMeasurements; i++) {
     const startTime = performance.now();
     callback(input, target);
     const endTime = performance.now();
     cumulative += endTime - startTime;
   }
-  return cumulative / num;
+  return cumulative / numberOfMeasurements;
 };
 
 module.exports = createDataPoint;
--- custom-algorithms/saveData.js
+++ custom-algorithms/saveData.js
@@ -3,14 +3,14 @@ const createDataPoint = require("./createDataPoint");
 
 const saveData = (
   callbackOne,
-  num,
+  numbeOfMeasurements,
   callbackTwo = generateArray,
   callbackThree = createDataPoint
 ) => {
   const data = [];
   for (let i = 1; i < 21; i++) {
     const array = callbackTwo(5000 * i);
-    const y = callbackThree(array, callbackOne, num, 10000);
+    const y = callbackThree(array, callbackOne, numbeOfMeasurements, 10000);
     data.push(y);
   }
   return data;
