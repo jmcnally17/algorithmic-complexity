222e40a956b8b7060522523b1afe0a70622b9407
Joe McNally <joe.t.mcnally@hotmail.com>
2022-07-02 10:48:38 +0100

    Tweak shuffle feature test
    Lower the increment for the shuffle feature test so that the test executes
    quicker.

--- custom-algorithms/shuffle/feature.test.js
+++ custom-algorithms/shuffle/feature.test.js
@@ -3,6 +3,6 @@ const saveData = require("../saveData");
 const shuffle = require("./shuffle");
 
 test("creates a set of 20 data points for timings of shuffle()", () => {
-  const data = saveData(shuffle, 100, 1000);
+  const data = saveData(shuffle, 100, 100);
   expect(data.length).toBe(20);
 });
